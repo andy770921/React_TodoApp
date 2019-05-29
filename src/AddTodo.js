34712a1b7d5c1c276925878a88161a552875a6cc
andy770921 <andy770921@yahoo.com.tw>
2019-05-29 17:55:32 +0800

    finish all

--- src/AddTodo.js
+++ src/AddTodo.js
@@ -14,19 +14,20 @@ class AddTodoUI extends Component {
         this.props.newTodo(this.state);
         //資料傳輸路徑: 此文件state -> App.js的newTodo property
         // -> addTodo函數的 (todo)
+        this.setState({
+            content : ''
+        });
     };
     render(){
         return (
             <div>
                 <form onSubmit={this.handleSubmit}>
                     <label>Add New Todos:</label>
-                    <input type="text" onChange={this.handleChange} />
+                    <input type="text" onChange={this.handleChange} value={this.state.content}/>
                 </form>
             </div>
         )
-
     }
-
 };
 
 export default AddTodoUI;
